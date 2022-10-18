e421f05c99f99ab6c7ee1ea90cb8f361602258db
Leo <leomutungi19@gmail.com>
2022-10-18 19:14:35 +0300

    completed assignment

---
+++ app.rb
@@ -0,0 +1,13 @@
+puts "Hello World!"
+print "Pass this test, please."
+# print "Hello world!"
+
+# puts [1, 2, 3]
+
+# puts [1, 2, 3].inspect
+
+p [1, 2, 3]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }].pretty_inspect
